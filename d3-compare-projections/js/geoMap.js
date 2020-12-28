13f354741f7298f73389dbe0c1453730ad9117f4
Michael Oppermann <michael@oppermann.at>
2020-12-28 14:58:55 -0800

    updated projections

--- d3-compare-projections/js/geoMap.js
+++ d3-compare-projections/js/geoMap.js
@@ -5,14 +5,15 @@ class GeoMap {
    * @param {Object}
    * @param {Array}
    */
-  constructor(_config, data) {
+  constructor(_config, _data) {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 600,
       containerHeight: _config.containerHeight || 400,
       margin: _config.margin || {top: 0, right: 0, bottom: 0, left: 0},
+      projection: _config.projection || d3.geoMercator()
     }
-    this.data = data;
+    this.data = _data;
     this.initVis();
   }
   
@@ -37,9 +38,8 @@ class GeoMap {
         .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
     //const projection = d3.geoMercator.fitSize([width, height], object);
-    vis.projection = d3.geoMercator();
     //vis.projection = d3.geoConicEqualArea();
-    vis.geoPath = d3.geoPath().projection(vis.projection);
+    vis.geoPath = d3.geoPath().projection(vis.config.projection);
 
     vis.renderVis();
   }
@@ -55,7 +55,7 @@ class GeoMap {
     const provinces = topojson.feature(vis.data, vis.data.objects.provinces)
 
     // Defines the scale of the projection so that the geometry fits within the SVG area
-    vis.projection.fitSize([vis.width, vis.height], provinces);
+    vis.config.projection.fitSize([vis.width, vis.height], provinces);
 
     // Append shapes of Canadian provinces
     const geoPath = vis.chart.selectAll('.geo-path')
--- d3-compare-projections/js/main.js
+++ d3-compare-projections/js/main.js
@@ -9,9 +9,21 @@ d3.json('data/canada_provinces.topo.json')
       projection: 'albersUsa'
     }, data);
     */
-    const geoMap2 = new GeoMap({ 
+    // Mercator projection
+    const geoMap1 = new GeoMap({ 
       parentElement: '#mercator',
-      projection: 'mercator'
+      projection: d3.geoMercator()
+    }, data);
+
+    // Lambert conformal conic projection
+    // See: https://observablehq.com/@bryik/statscans-most-common-map-projection
+    // We need to rotate the globe. You can often find specifications for popular projections
+    // and world regions somewhere on the internet or you tweak the parameters to get a satisfying result.
+    const geoMap2 = new GeoMap({ 
+      parentElement: '#lambert',
+      projection: d3.geoConicConformal()
+          .parallels([49, 77])
+          .rotate([91.86667, 0])
     }, data);
   })
   .catch(error => console.error(error));
